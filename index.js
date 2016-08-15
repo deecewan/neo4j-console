83c217eae8556854b3dba85b724be348eb93f068
Itai Steinherz <itaisteinherz@gmail.com>
2016-08-15 12:26:43 +0300

    Added minimum working code
    Added the runStatement function for the client to encode and send the statement to the server. Then, the server decodes and runs the statement through the db module and serves the result back to the client.

---
+++ index.js
@@ -0,0 +1,27 @@
+const db = require('db')({
+    username: "neo4j",
+    password: "admin"
+});
+const express = require('express');
+
+var app = express(),
+    server = app.listen(3000, "0.0.0.0", function() {
+        console.log('Listening on *:3000');
+    });
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/statement/:statement', function(req, res, next) { // TODO: Send this information back to the index.html file, so the user can execute another statement.
+    runStatement(decodeURIComponent(req.params.statement), function(result) {
+        res.send(`<pre>${JSON.stringify(result.records, null, 4).split("\\\"").join("")}</pre>`);
+        next();
+    }, function(err) {
+        next(`Got an error trying to run the statement:\n${err}`);
+    });
+});
+
+function runStatement(statement, resolve, reject) {
+    db.runStatement(statement, resolve, reject);
+}
