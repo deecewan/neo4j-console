390d43c477ffe5dc7b1da0e2e60a16532ae308e4
Itai Steinherz <itaisteinherz@gmail.com>
2016-08-21 10:11:49 +0300

    Deleted unnecessary function and code
    Deleted the runStatement function in the index.js file, and added better error logging.
    In db/index.js, added support for parameters, in case I would like to use them in the future.

--- index.js
+++ index.js
@@ -15,14 +15,11 @@ app.get('/', function(req, res) {
 });
 
 app.get('/statement/:statement', function(req, res, next) { // TODO: Send this information back to the index.html file, so the user can execute another statement.
-    runStatement(decodeURIComponent(req.params.statement), function(result) {
+    db.runStatement(decodeURIComponent(req.params.statement), {}, function(result) {
         res.send(`<pre>${JSON.stringify(result.records, null, 4).split("\\\"").join("")}</pre>`);
         next();
     }, function(err) {
         next(`Got an error trying to run the statement:\n${err}`);
+        res.send(`<pre>${JSON.stringify(err, null, 4).split("\\\"").join("")}</pre>`);
     });
 });
-
-function runStatement(statement, resolve, reject) {
-    db.runStatement(statement, resolve, reject);
-}
