5bed83224829757f3af894956c202b3dbe5e82fb
Itai Steinherz <itaisteinherz@gmail.com>
2016-12-20 16:13:36 +0200

    Update test/index.js
    Improved the test/index.js file.

--- test/index.js
+++ test/index.js
@@ -9,7 +9,9 @@ describe("Neo4j Console", function() { // TODO: Add tests for all of the feature
     describe("/statement/:statement", function() {
         it("should run the given statement and return the result", function() {
             return new Promise(function(resolve, reject) {
-                http.get(`http://localhost:3000/statement/${encodeURIComponent("CREATE (n:TestNode {name: \"test\"}) WITH n.name AS name, n as TestNode DELETE TestNode RETURN name")}`, (res) => {
+                const testURL = `http://localhost:3000/statement/${encodeURIComponent("CREATE (n:TestNode {name: \"test\"}) WITH n.name AS name, n as TestNode DELETE TestNode RETURN name")}`;
+
+                http.get(testURL, (res) => {
                     const statusCode = res.statusCode;
                     assert.equal(statusCode, 200);
 
@@ -20,7 +22,7 @@ describe("Neo4j Console", function() { // TODO: Add tests for all of the feature
                     });
                     res.on("end", function() {
                         try {
-                            assert.equal(rawData, testOutput);
+                            assert.equal(rawData.toString(), testOutput);
                             resolve();
                         } catch (e) {
                             reject(e);
