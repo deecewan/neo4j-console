32680e2084ca6a000714700a9b8c0525d3041569
Itai Steinherz <itaisteinherz@gmail.com>
2016-12-21 11:43:40 +0200

    Update test/index.js
    Fixed issues in the test/index.js file so the tests will work.

--- test/index.js
+++ test/index.js
@@ -3,8 +3,6 @@
 const assert = require("assert"),
     http = require("http");
 
-const testOutput = `{"result":{"records":[{"keys":["name"],"length":1,"_fields":["test"],"_fieldLookup":{"name":0}}],"summary":{"statement":{"text":"CREATE (n:TestNode {name: \\"test\\"}) WITH n.name AS name, n as TestNode DELETE TestNode RETURN name","parameters":{}},"statementType":"rw","updateStatistics":{"_stats":{"nodesCreated":1,"nodesDeleted":1,"relationshipsCreated":0,"relationshipsDeleted":0,"propertiesSet":1,"labelsAdded":1,"labelsRemoved":0,"indexesAdded":0,"indexesRemoved":0,"constraintsAdded":0,"constraintsRemoved":0}},"plan":false,"profile":false,"notifications":[]}}}`;
-
 describe("Neo4j Console", function() { // TODO: Add tests for all of the features of the project.
     before("set database password", function() {
         return new Promise(function(resolve, reject) {
@@ -16,7 +14,6 @@ describe("Neo4j Console", function() { // TODO: Add tests for all of the feature
 
                 resolve();
             }).on("error", (e) => {
-                console.error(`Error: ${e.message}.\n\n${e}`);
                 reject(e);
             });
         });
@@ -36,16 +33,18 @@ describe("Neo4j Console", function() { // TODO: Add tests for all of the feature
                     res.on("data", function(chunk) {
                         rawData += chunk;
                     });
+
                     res.on("end", function() {
                         try {
-                            assert.equal(rawData.toString(), testOutput);
+                            rawData = JSON.parse(rawData);
+
+                            assert.equal(rawData.result.records[0]["_fields"][0], "test");
                             resolve();
                         } catch (e) {
                             reject(e);
                         }
                     });
                 }).on("error", (e) => {
-                    console.error(`Error: ${e.message}.\n\n${e}`);
                     reject(e);
                 });
             });
